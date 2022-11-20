e16cfb362eaad43721249faacf29999fe8cd608a
Manikandan S <MohanRK1893>
2022-11-20 13:04:54 +0530

    deyail

---
+++ src/org/test/B.java
@@ -0,0 +1,25 @@
+package org.test;
+
+public class B extends Test  {
+	
+	private void m2() {
+		Test.m1();
+		System.out.println(Test.m1());
+	}
+	
+	public static void main(String[] args) {
+		
+		B.m();
+		
+	}
+	
+	
+		}
+	
+	
+	
+	
+	
+	
+
+
---
+++ src/org/test/Test.java
@@ -0,0 +1,42 @@
+package org.test;
+
+
+
+public class Test {
+
+	static int a=10;
+		
+		private void m1() {
+			a=100;
+
+         System.out.println(a);		
+		}
+		
+	private void m2() {
+			
+			System.out.println(a);
+		}
+		
+		public static void main(String[] args)
+		{
+		Test T1 =new Test();
+		T1.m1();
+		T1.m2();
+		Test t = new Test();
+		t.m2();
+		t.m1();
+		
+		}
+				
+				
+				
+			}
+
+	
+		
+	
+	
+	
+	
+	
+	
